d59e26a3ad9d8a2f5b459f47932db66d14df18a4
maksb <maksbolshy@mail.ru>
2024-02-19 18:27:12 +0300

    Initial commit

---
+++ src/main/java/ru/gb/spring/example7hw1/Example7hw1Application.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.example7hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example7hw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example7hw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/spring/example7hw1/Example7hw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.example7hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example7hw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
